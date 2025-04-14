471e3bd75164d10553853907266a31155b82e3c9
Sajad pourajam <prj.sajad85@gmail.com>
2025-04-14 16:41:30 +0330

    C formatter

--- formatter.lua
+++ formatter.lua
@@ -1,14 +1,16 @@
-    require("conform").setup({
-      formatters_by_ft = {
-        python = { "black" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        php = { "php_cs_fixer" },
-        rust = { "rustfmt" },
-        go = { "gofmt" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    })
+require("conform").setup({
+  formatters_by_ft = {
+    python = { "black" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    php = { "php_cs_fixer" },
+    rust = { "rustfmt" },
+    go = { "gofmt" },
+    c = { "clang-format" },  
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
+
