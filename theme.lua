26d874b7a25023919202fa6c75d79bfc4f029722
Sajad pourajam <prj.sajad85@gmail.com>
2025-08-02 20:56:06 +0330

    Update

--- theme.lua
+++ theme.lua
@@ -5,6 +5,29 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,  
   },
 }
+local transparent_enabled = true
 
-vim.cmd("colorscheme dayfox")
- 
+require("nightfox").setup({
+  options = {
+    transparent = true,  -- 👈 فعال کردن شفافیت
+  }
+})
+
+vim.cmd("colorscheme nightfox")
+
+local function toggle_transparency()
+  transparent_enabled = not transparent_enabled
+
+  require("nightfox").setup({
+    options = {
+      transparent = transparent_enabled,
+    },
+  })
+
+  vim.cmd("colorscheme nightfox")
+
+
+end
+
+vim.keymap.set("n", "<F5>", toggle_transparency, { noremap = true, silent = true })
+
